42d0da3c9adbdfa2ad6837a219eadd6c313deae5
Nami Izarra <nami.izarra@gmail.com>
2020-01-08 17:02:29 +0900

    got map to work with leaflet finally

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,31 +1,35 @@
 import React from 'react';
 import FlatList from './flatList';
 import flats from '../data/flats';
-import GoogleMapReact from 'google-map-react';
-
-const REACT_APP_MAP_API_KEY = `${process.env.REACT_APP_MAP_API_KEY}`;
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
 
 class App extends React.Component {
-  static defaultProps = {
-     center: {lat: 40.73, lng: -73.93},
-     zoom: 12
+  constructor(props){
+    super(props);
+
+    this.state = {
+      lat: 51.505,
+      lng: -0.09,
+      zoom: 13
+    };
   }
 
   render() {
+    const position = [this.state.lat, this.state.lng]
     return(
       <div>
         <FlatList flatList={flats} />
-        <div className="map-container">
-          <GoogleMapReact
-            bootstrapURLKeys={{
-               key: REACT_APP_MAP_API_KEY,
-               language: 'en'
-            }}
-            defaultCenter={this.props.center}
-            defaultZoom={this.props.zoom}
-          >
-          </GoogleMapReact>
-        </div>
+        <Map center={position} zoom={this.state.zoom}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='https://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          <Marker position={position}>
+            <Popup>
+              A pretty CSS3 popup. <br/> Easily customizable.
+            </Popup>
+          </Marker>
+        </Map>
       </div>
     )
   }
